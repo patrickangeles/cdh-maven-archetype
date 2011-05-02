6258357396441e94cdc2ad7d59a37aecf5b171de
patrick <patrick@cloudera.com>
2011-05-01 20:27:39 -0400

    License headers and notice text.

--- create-deploy-file.sh
+++ create-deploy-file.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 #
+# Copyright (c) 2011, Cloudera, Inc. All Rights Reserved.
+#
+# Cloudera, Inc. licenses this file to you under the Apache License,
+# Version 2.0 (the "License"). You may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# This software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for
+# the specific language governing permissions and limitations under the
+# License.
+#
+
+#
 # Script to create and deploy an archetype from an existing mvn project to a local directory.
 # Full (not relative) paths should be used for the LOCAL_PATH argument.
 #
